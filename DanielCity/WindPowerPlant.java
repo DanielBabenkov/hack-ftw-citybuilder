0bfc972f7c1f69001b7b6658aafd7b437d28d250
Wyatt Woker <35244096+wjwoker@users.noreply.github.com>
2020-02-01 14:23:29 -0600

    Added changes from dub-stuff

---
+++ DanielCity/WindPowerPlant.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.BasicStroke;
+import java.awt.Graphics2D;
+
+/**
+ * A Wind Power Plant
+ *
+ * @author DanielBabenkov
+ * @version 1 February 2020
+ */
+public class WindPowerPlant extends Building
+{
+    private static final long DAILY_PRODUCTION_IN_MWH = 70;
+    private static final long COST_PER_MWH = 41;
+    private static final double EMISSIONS_PER_MWH = 0.011;
+    
+    public WindPowerPlant(int leftX, int bottomY) {
+        super(0, DAILY_PRODUCTION_IN_MWH, EMISSIONS_PER_MWH, COST_PER_MWH, bottomY, leftX, 20, 20);
+    }
+    public void draw(Graphics2D g2) {
+        g2.setColor(Color.DARK_GRAY);
+        Rectangle plant = new Rectangle(getBottomY() - 20, getLeftX(), getWidth(), getHeight());
+        g2.fill(plant);
+    }
+}
